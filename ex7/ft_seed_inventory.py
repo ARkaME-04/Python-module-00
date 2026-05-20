98ea6c37776b133e7a1296fa1c3ca3d272ba7ff4
Dexter <rhyanclarck123@gmail.com>
2026-05-20 09:07:58 +0300

    Fix capitalization in ft_seed_inventory function

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,15 +1,12 @@
-from numpy.core.defchararray import capitalize
-
-
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     if unit == "packets":
-        print(f"{capitalize(seed_type)} seeds:"
+        print(f"{seed_type.capitalize()} seeds:"
               f" {quantity} packets available")
     elif unit == "grams":
-        print(f"{capitalize(seed_type)} seeds:"
+        print(f"{seed_type.capitalize()} seeds:"
               f" {quantity} grams total")
     elif unit == "area":
-        print(f"{capitalize(seed_type)}"
+        print(f"{seed_type.capitalize()}"
               f" seeds: covers {quantity} square meters")
     else:
         print("Unknown unit type")
